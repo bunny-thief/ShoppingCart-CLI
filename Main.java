8c3d011ddb3992801675b3916a29bb0b7557af77
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-24 14:44:02 +0200

    Add removeItemFromList() method

--- Main.java
+++ Main.java
@@ -32,6 +32,18 @@ class ShoppingCart {
 
             if (choice == 1) {
                 addItemToList();
+                continue;
+            }
+
+            if (choice == 2) {
+                String removed = removeItemFromList();
+
+                if (removed.equals("Item was not found!")) {
+                    System.out.println(removed);
+                } else {
+                    System.out.printf("%d was removed from the shoppint cart.");
+                }
+                continue;
             }
         }
 
@@ -46,4 +58,15 @@ class ShoppingCart {
         System.out.printf("%s was added to the shopping card.", newItem);
     }
 
+    public String removeItemFromList() {
+        System.out.println("Enter the item to remove:");
+        String itemToRemove = scanner.nextLine();
+
+        for (int i = 0; i < shoppingCart.size(); i++) {
+            if (itemToRemove.equals(shoppingCart.get(i))) {
+                return shoppingCart.remove(i);
+            }
+        }
+        return "Item was not found!";
+    }
 }
