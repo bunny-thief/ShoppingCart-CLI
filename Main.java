d745aae909cf81b404064c660414441e5605b8b3
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-08-24 14:18:45 +0200

    Add ShoppingCart class

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class ShoppingCart {
+    ArrayList<String> shoppingCart;
+    Scanner scanner;
+
+    public ShoppingCart(ArrayList<String> shoppingCart, Scanner scanner) {
+        this.shoppingCart = shoppingCart;
+        this.scanner = scanner;
+    }
 
 }
